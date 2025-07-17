b4f03beefe8b726d8ba0475ec7e3e1d457007a4b
Zhuravleva Gelena <roselifemeow@gmail.com>
2025-07-17 17:51:54 +0300

    made stringIntConv, ended 1st week of trainee

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type StringToInt interface {
+	StringToIntConv([]string) []int
+}
+
+type StringConversation struct {
+}
+
+func (sс StringConversation) StringToIntConv(arr []string) []int {
+	result := make([]int, 0, len(arr))
+
+	for _, str := range arr {
+		result = append(result, strings.Count(str, "x"))
+		// result[id] = strings.Count(str, "x")
+	}
+	return result
+}
 
 func main() {
-	fmt.Println("Hello world")
+
+	arr := [...]string{"xxx", "meow", "meow", "i love x", "deepseek"}
+	converter := StringConversation{}
+	result := converter.StringToIntConv(arr[:])
+	fmt.Println(result)
 }
